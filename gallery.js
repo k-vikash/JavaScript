24ca18b6ff1aadf13f3508c9221b6cb103699d46
Vikash Kumar <vikash.kumar@zemosolabs.com>
2017-09-20 15:00:10 +0530

    Initial commit

---
+++ gallery.js
@@ -0,0 +1,53 @@
+
+var arr=([
+{
+	"imgId":"image01",
+	"url":"imgs/8.jpg",
+	"caption": "logo"
+},
+{
+	"imgId":"image02",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image03",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image04",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image05",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image06",
+		"url":"imgs/8.jpg",
+	"caption": "logo"
+},
+{
+	"imgId":"image07",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image08",
+		"url":"imgs/8.jpg",
+	"caption": "logo"
+},
+{
+	"imgId":"image09",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+},
+{
+	"imgId":"image10",
+		"url":"imgs/8.jpg",
+	"caption": "logo"	
+}]
+)
